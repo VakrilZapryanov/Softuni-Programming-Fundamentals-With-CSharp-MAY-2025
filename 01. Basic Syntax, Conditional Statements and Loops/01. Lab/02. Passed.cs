97fb15000c928bc1515d3024e6055c0d94f0d74f
VakrilZapryanov <vakrill.zapryanov@gmail.com>
2025-05-14 23:54:12 +0300

    Fundamentals

---
+++ 01. Basic Syntax, Conditional Statements and Loops/01. Lab/02. Passed.cs
@@ -0,0 +1,6 @@
+﻿double grade = double.Parse(Console.ReadLine());
+
+if (grade >= 3.0)
+{
+    Console.WriteLine("Passed!");
+}
---
+++ 01. Basic Syntax, Conditional Statements and Loops/01. Lab/07. Theatre Promotion.cs
@@ -0,0 +1,39 @@
+﻿string dayType = Console.ReadLine();
+var age = int.Parse(Console.ReadLine());
+
+var price = 0;
+
+if (age >= 0 && age <= 18)
+{
+    if (dayType == "Weekday")
+        price = 12;
+    else if (dayType == "Weekend")
+        price = 15;
+    else
+        price = 5;
+}
+else if (age > 18 && age <= 64)
+{
+    if (dayType == "Weekday")
+        price = 18;
+    else if (dayType == "Weekend")
+        price = 20;
+    else
+        price = 12;
+}
+else if (age > 64 && age <= 122)
+{
+    if (dayType == "Weekday")
+        price = 12;
+    else if (dayType == "Weekend")
+        price = 15;
+    else
+        price = 10;
+}
+
+if (age < 0 || age > 122)
+{
+    Console.WriteLine("Error!");
+}
+else
+    Console.WriteLine(price + "$");
