5a270a52deeb9ab3242b7b69c088c5d5c3ecca87
VakrilZapryanov <vakrill.zapryanov@gmail.com>
2025-05-20 18:18:23 +0300

    Fundamentals

---
+++ 01. Basic Syntax, Conditional Statements and Loops/02. Exercise/03.Vacation.cs
@@ -0,0 +1,64 @@
+﻿//Input
+int peopleCount = int.Parse(Console.ReadLine());
+string groupType = Console.ReadLine();
+string day = Console.ReadLine();
+
+//Calculations
+double priceForOne = 0;
+double totalPrice = 0;
+
+if (groupType == "Students")
+{
+    switch (day)
+    {
+        case ("Friday"): priceForOne = 8.45; break;
+        case ("Saturday"): priceForOne = 9.8; break;
+        case ("Sunday"): priceForOne = 10.46; break;
+    }
+
+    totalPrice = priceForOne * peopleCount;
+
+    //For Students – if the group is 30 or more people, you should reduce the total price by 15%.
+    if (peopleCount >= 30)
+    {
+        totalPrice -= totalPrice * 0.15;
+    }
+}
+else if (groupType == "Business")
+{
+    switch (day)
+    {
+        case ("Friday"): priceForOne = 10.9; break;
+        case ("Saturday"): priceForOne = 15.6; break;
+        case ("Sunday"): priceForOne = 16; break;
+    }
+
+    totalPrice = priceForOne * peopleCount;
+
+    //For Business – if the group is 100 or more people, 10 of the people stay for free.
+    if (peopleCount >= 100)
+    {
+        totalPrice -= priceForOne * 10;
+    }
+}
+else if (groupType == "Regular")
+{
+
+    switch (day)
+    {
+        case ("Friday"): priceForOne = 15; break;
+        case ("Saturday"): priceForOne = 20; break;
+        case ("Sunday"): priceForOne = 22.5; break;
+    }
+
+    totalPrice = priceForOne * peopleCount;
+
+    //For Regular – if the group is between 10 and 20 people (both inclusively), reduce the total price by 5%.
+    if (peopleCount >= 10 && peopleCount <= 20)
+    {
+        totalPrice -= totalPrice * 0.05;
+    }
+}
+
+//Output
+Console.WriteLine($"Total price: {totalPrice:f2}");
---
+++ 01. Basic Syntax, Conditional Statements and Loops/02. Exercise/04.Print and sum.cs
@@ -0,0 +1,17 @@
+﻿//Input
+int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+
+//Calculations
+int sum = 0;
+
+for (int currentNum = start; currentNum <= end; currentNum++)
+{
+    Console.Write(currentNum + " "); //Output
+    sum += currentNum;
+}
+
+Console.WriteLine(); //Next row
+
+//Output
+Console.WriteLine("Sum: " + sum);
